e0da08301cd5b8a627c94a6744c3a68592e6346a
redkaz <maksimkcenk93@gmail.com>
2023-09-25 23:49:11 +0300

    Complete app

--- app/main.py
+++ app/main.py
@@ -1,200 +1,196 @@
-import psycopg2
 import json
+import psycopg2
+import re
+
 from wsgiref.simple_server import make_server
-import configparser
 from database import create_connection
-config = configparser.ConfigParser()
-config.read('config.ini')
 
-# import re
-# pattern = r'\s*([А-Яа-яЁё]+)(\d+)\s*'
-# string = r'---   Опять45   ---'
-# match = re.search(pattern, string)
 
-class MyApp:
+class ResourceApp:
     def __init__(self):
         self.conn = create_connection()
 
-    # def __del__(self):
-    #     self.conn.close()
+    def get_data(self, id=None):
+        with self.conn.cursor() as cursor:
+            try:
+                if id:
+                    cursor.execute(
+                        'SELECT r.*, t.max_speed FROM resource r '
+                        'INNER JOIN resource_type t ON r.type_id = t.id '
+                        'WHERE r.id = %s',
+                        (id,)
+                    )
+                else:
+                    cursor.execute(
+                        'SELECT r.*, t.max_speed FROM resource r '
+                        'INNER JOIN resource_type t ON r.type_id = t.id'
+                    )
+                columns = [desc[0] for desc in cursor.description]
+                rows = cursor.fetchall()
+                json_data = []
+                for row in rows:
+                    resource = dict(zip(columns, row))
+                    resource['exceed_percentage'] = (
+                            (resource['current_speed'] - resource['max_speed']) / resource['max_speed'] * 100
+                    )
+                    json_data.append(resource)
+                return json_data
+            except (Exception, psycopg2.DatabaseError) as e:
+                return {'error': str(e)}
 
-    def get_type(self):
-        cursor = self.conn.cursor()
-        cursor.execute('SELECT * FROM resource_type')
-        json_data = []
+    def get_data_filtered_by_type(self, type_id):
+        with self.conn.cursor() as cursor:
+            try:
+                cursor.execute('SELECT * FROM resource WHERE type_id = %s', (type_id,))
+                columns = [desc[0] for desc in cursor.description]
+                json_data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+                return json_data
+            except (Exception, psycopg2.DatabaseError) as e:
+                return {'error': str(e)}
 
-        columns = [desc[0] for desc in cursor.description]
+    def create_data(self, data):
+        with self.conn.cursor() as cursor:
+            try:
+                cursor.execute(
+                    'INSERT INTO resource (type_id, name, current_speed) VALUES (%s, %s, %s)',
+                    (data['type_id'], data['name'], data['current_speed'])
+                )
+                self.conn.commit()
+            except (Exception, psycopg2.DatabaseError) as e:
+                return {'error': str(e)}
 
-        for row in cursor.fetchall():
-            row_data = dict(zip(columns, row))
-            json_data.append(row_data)
-        cursor.close()
+    def update_data(self, id, data):
+        with self.conn.cursor() as cursor:
+            try:
+                cursor.execute(
+                    'UPDATE resource SET type_id = %s, name = %s, current_speed = %s WHERE id = %s',
+                    (data['type_id'], data['name'], data['current_speed'], id)
+                )
+                self.conn.commit()
+            except (Exception, psycopg2.DatabaseError) as e:
+                return {'error': str(e)}
 
-        return json_data
+    def delete_data(self, id):
+        with self.conn.cursor() as cursor:
+            try:
+                cursor.execute('DELETE FROM resource WHERE id = %s', (id,))
+                self.conn.commit()
+            except (Exception, psycopg2.DatabaseError) as e:
+                return {'error': str(e)}
 
 
-# dict = {
-#     type: 'resourse_type',
-#     resource: 'resource'
-# }
-    def create_type(self, data):
-        cursor = self.conn.cursor()
-        cursor.execute(
-            'INSERT INTO resource_type (id, name, max_speed) VALUES (%s, %s, %s)',
-            (data['id'], data['name'], data['max_speed'])
-        )
-        self.conn.commit()
-        cursor.close()
-        self.conn.close()
-#
-#     def update_type(self, data):
-#         cursor = self.conn.cursor()
-#         cursor.execute(
-#             'UPDATE resource_type SET name = %s, max_speed = %s WHERE id = %s',
-#             (data['name'], data['max_speed'], data['id'])
-#         )
-#         self.conn.commit()
-#         cursor.close()
-#
-#     def delete_type(self, data):
-#         cursor = self.conn.cursor()
-#         cursor.execute(
-#             'DELETE FROM resource_type WHERE id = %s',
-#             (data['id'],)
-#         )
-#         self.conn.commit()
-#         cursor.close()
+class TypeApp:
+    def __init__(self):
+        self.conn = create_connection()
 
-# urls = [
-#     r'/type/<id>','GET', route - типизиваронная вьюха. Кидает 400, если тип не сошелся
-# ]
+    def get_data(self, id=None):
+        with self.conn.cursor() as cursor:
+            try:
+                if id:
+                    cursor.execute('SELECT * FROM resource_type WHERE id = %s', (id,))
+                else:
+                    cursor.execute('SELECT * FROM resource_type')
+                columns = [desc[0] for desc in cursor.description]
+                json_data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+                return json_data
+            except (Exception, psycopg2.DatabaseError) as e:
+                return {'error': str(e)}
+
+    def create_data(self, data):
+        with self.conn.cursor() as cursor:
+            try:
+                cursor.execute(
+                    'INSERT INTO resource_type (name, max_speed) VALUES (%s, %s)',
+                    (data['name'], data['max_speed'])
+                )
+                self.conn.commit()
+            except (Exception, psycopg2.DatabaseError) as e:
+                return {'error': str(e)}
+
+    def update_data(self, id, data):
+        with self.conn.cursor() as cursor:
+            try:
+                cursor.execute(
+                    'UPDATE resource_type SET name = %s, max_speed = %s WHERE id = %s',
+                    (data['name'], data['max_speed'], id)
+                )
+                self.conn.commit()
+            except (Exception, psycopg2.DatabaseError) as e:
+                return {'error': str(e)}
+
+    def delete_data(self, id):
+        with self.conn.cursor() as cursor:
+            try:
+                cursor.execute('DELETE FROM resource_type WHERE id = %s', (id,))
+                self.conn.commit()
+            except (Exception, psycopg2.DatabaseError) as e:
+                return {'error': str(e)}
+
+
+resource_app = ResourceApp()
+type_app = TypeApp()
+
+routes = [
+    ('GET', r'/resource/(\d+)', resource_app, 'get_data'),
+    ('POST', r'/resource', resource_app, 'create_data'),
+    ('PUT', r'/resource/(\d+)', resource_app, 'update_data'),
+    ('DELETE', r'/resource/(\d+)', resource_app, 'delete_data'),
+    ('GET', r'/type/(\d+)', type_app, 'get_data'),
+    ('POST', r'/type', type_app, 'create_data'),
+    ('PUT', r'/type/(\d+)', type_app, 'update_data'),
+    ('DELETE', r'/type/(\d+)', type_app, 'delete_data'),
+    ('GET', r'/resource/filter_by_type/(\d+)', resource_app, 'get_data_filtered_by_type'),
+]
+
 
 def application(environ, start_response):
-
-
-    # for path,method, route in:
-    # environ['PATH_INFO'] == path and method == environ['REQUEST_METHOD']
-    # match = re.search(path,environ['PATH_INFO'])
-    # route(match) получить кварги
-    app = MyApp()
     response_body = {}
     status = '200 OK'
+    response_headers = [('Content-type', 'application/json')]
 
-    request_method = environ['REQUEST_METHOD']
     path = environ['PATH_INFO']
+    request_method = environ['REQUEST_METHOD']
 
-    if request_method == 'GET':
-        if path == '/get_type':
-            data = app.get_type()
-            response_body = json.dumps(data)
-            # status = '200 OK'
-
-    elif request_method == 'POST':
-        if path == '/create_type':
-            request_body = environ['wsgi.input'].read()
-            data = json.loads(request_body.decode('utf-8'))
-            app.create_type(data)
-            response_body = json.dumps({'message': 'New type created successfully'})
-            status = '201 Created'
-    #
-    # elif request_method == 'PUT':
-    #     if path == '/update_data':
-    #         request_body = environ['wsgi.input'].read()
-    #         data = json.loads(request_body.decode('utf-8'))
-    #         app.update_type(data)
-    #         response_body = json.dumps({'message': 'Type updated successfully'})
-    #
-    # elif request_method == 'DELETE':
-    #     if path == '/delete_type':
-    #         request_body = environ['wsgi.input'].read()
-    #         data = json.loads(request_body.decode('utf-8'))
-    #         app.delete_type(data)
-    #         response_body = json.dumps({'message': 'Type deleted successfully'})
-
+    for method, pattern, class_instance, func_name in routes:
+        if request_method == 'GET' and re.match(r'/resource/filter_by_type/(\d+)', path):
+            try:
+                match = re.match(r'/resource/filter_by_type/(\d+)', path)
+                type_id = int(match.group(1))
+                handler = resource_app.get_data_filtered_by_type
+                response_body = json.dumps(handler(type_id))
+            except ValueError as ve:
+                response_body = json.dumps({'error': str(ve)})
+                status = '400 Bad Request'
+            except Exception as e:
+                response_body = json.dumps({'error': str(e)})
+                status = '500 Internal Server Error'
+        if request_method == method and re.match(pattern, path):
+            try:
+                match = re.match(pattern, path)
+                if match.groups():
+                    args = [int(arg) for arg in match.groups()]
+                    handler = getattr(class_instance, func_name)
+                    response_body = json.dumps(handler(*args))
+                else:
+                    data = json.loads(environ['wsgi.input'].read(int(environ.get('CONTENT_LENGTH', 0))))
+                    handler = getattr(class_instance, func_name)
+                    response_body = json.dumps(handler(data))
+            except ValueError as ve:
+                response_body = json.dumps({'error': str(ve)})
+                status = '400 Bad Request'
+            except Exception as e:
+                response_body = json.dumps({'error': str(e)})
+                status = '500 Internal Server Error'
+            break
     else:
-        response_body = json.dumps({'error': 'Invalid request method'})
+        response_body = json.dumps({'error': 'Invalid request method or path'})
         status = '400 Bad Request'
 
-    response_headers = [('Content-type', 'application/json')]
     start_response(status, response_headers)
-    print(response_body)
-
-    return response_body
+    return [response_body.encode('utf-8')]
 
 
 if __name__ == '__main__':
     with make_server('', 8000, application) as httpd:
-        print("Serving on port 8000...")
+        print('Serving on port 8000...')
         httpd.serve_forever()
-
-
-# class MyDB:
-#     def __init__(self,name):
-#         self.name = name
-#         self.conn = psycopg2.connect(
-#             database=config['database']['DATABASE_NAME'],
-#             user=config['database']['DATABASE_USER'],
-#             password=config['database']['DATABASE_PASSWORD'],
-#             host=config['database']['DATABASE_HOST'],
-#             port=config['database']['DATABASE_PORT']
-#         )
-#
-#     def __del__(self):
-#         self.conn.close()
-#
-#     def get_type(self,**filters):
-#         cursor = self.conn.cursor()
-#         cursor.execute('SELECT * FROM resource_type')
-#         json_data = []
-#
-#         columns = [desc[0] for desc in cursor.description]
-#
-#         for row in cursor.fetchall():
-#             row_data = dict(zip(columns, row))
-#             json_data.append(row_data)
-#         cursor.close()
-#
-#         return json_data
-#
-#
-#     def create(self, data):
-#         cursor = self.conn.cursor()
-#         cursor.execute(
-#             f'INSERT INTO {self.name} ({data.keys().join(",")}) VALUES ({data.values().join(",")})',
-#             (data.keys().join(','),data.values().join(','))
-#         )
-#         self.conn.commit()
-#
-#         return self.conn.close()
-#
-#     def update_type(self, data):
-#         cursor = self.conn.cursor()
-#         cursor.execute(
-#             'UPDATE resource_type SET name = %s, max_speed = %s WHERE id = %s',
-#             (data['name'], data['max_speed'], data['id'])
-#         )
-#         self.conn.commit()
-#         cursor.close()
-#
-#     def update_many(self, data):
-#             cursor = self.conn.cursor()
-#             cursor.execute(
-#                 'UPDATE resource_type SET name = %s, max_speed = %s WHERE id = %s',
-#                 (data['name'], data['max_speed'], data['id'])
-#             )
-#             self.conn.commit()
-#             cursor.close()
-#
-#     def delete_type(self, data):
-#         cursor = self.conn.cursor()
-#         cursor.execute(
-#             'DELETE FROM resource_type WHERE id = %s',
-#             (data['id'],)
-#         )
-#         self.conn.commit()
-#         cursor.close()
-#
-#
-#
-# type = MyDB('resource_type')
-
